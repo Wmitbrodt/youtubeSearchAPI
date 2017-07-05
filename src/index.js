87ac690cca62a9989c66839f873fa88e8fc10d1b
Will <wmitbrodt@gmail.com>
2017-07-04 23:54:51 -0700

    changed functional component to class component, updated its state

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDrLw8yro0C8qH8SznqNuIrQYv5lCt0irk';
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
 
 // Create a new component. This component should
 // produce some HTML
-const App = () => {
-  return ( // if you don't use parenthesis here, make sure the <div> is on the first line, beside the return
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // returns the list of videos
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      //console.log(videos);
+      // we can update the state with the new list of videos:
+      //this.setState({ videos: videos });
+      // when the key and value are the same e.g., videos: videos, we can shorten with ES6 syntax: videos
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return ( // if you don't use parenthesis here, make sure the <div> is on the first line, beside the return
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+
 }
 
 // Take this component's generated HTML and put it
