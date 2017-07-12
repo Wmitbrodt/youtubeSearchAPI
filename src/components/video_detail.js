a01d92da91dbda5f0220ea7bb587fbe182df4cbb
Will <wmitbrodt@gmail.com>
2017-07-11 21:57:09 -0700

    put video detail component together

---
+++ src/components/video_detail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId;
+  // every youtube url is the same, except for the video id at the very end:
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url}></iframe>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  )
+
+};
+
+
+export default VideoDetail;
